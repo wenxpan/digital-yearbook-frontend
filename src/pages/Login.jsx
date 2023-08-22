99bdc2c228948e69eb323a8700514dcee3efad2c
wenxuan-pan <panwenxuan42@gmail.com>
2023-08-22 10:51:39 +1000

    added login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,22 +1,45 @@
-import React from "react"
+import React, { useState } from "react"
 import Button from "react-bootstrap/Button"
 import Form from "react-bootstrap/Form"
+import { useNavigate } from "react-router-dom"
 
 const Login = () => {
+  const nav = useNavigate()
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  function handleSubmit(e) {
+    console.log(email, password)
+  }
+
   return (
     <Form className="col-md-5">
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control
+          type="email"
+          placeholder="Enter email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </Form.Group>
-      <Button variant="secondary">Reset Password</Button>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
+      <div className="d-flex justify-content-around flex-wrap">
+        <Button variant="secondary" onClick={() => nav("/login/reset")}>
+          Reset Password
+        </Button>
+        <Button variant="primary" onClick={(e) => handleSubmit(e)}>
+          Log In
+        </Button>
+      </div>
     </Form>
   )
 }
