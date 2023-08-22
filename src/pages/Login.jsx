55b2691a1356b151944eff8254d524a69747ffce
wenxuan-pan <panwenxuan42@gmail.com>
2023-08-22 10:58:36 +1000

    added background to login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,34 +13,37 @@ const Login = () => {
   }
 
   return (
-    <Form className="col-md-5">
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control
-          type="email"
-          placeholder="Enter email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </Form.Group>
-      <div className="d-flex justify-content-around flex-wrap">
-        <Button variant="secondary" onClick={() => nav("/login/reset")}>
-          Reset Password
-        </Button>
-        <Button variant="primary" onClick={(e) => handleSubmit(e)}>
-          Log In
-        </Button>
-      </div>
-    </Form>
+    <>
+      <Form className="col-md-5 bg-dark px-5 py-3 bg-opacity-50 rounded">
+        <h1 className="mb-5">Log In</h1>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <div className="d-flex justify-content-around flex-wrap">
+          <Button variant="secondary" onClick={() => nav("/login/reset")}>
+            Reset Password
+          </Button>
+          <Button variant="primary" onClick={(e) => handleSubmit(e)}>
+            Log In
+          </Button>
+        </div>
+      </Form>
+    </>
   )
 }
 
