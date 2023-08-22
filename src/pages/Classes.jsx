bef89793d0878a84b249c4a91e287fcdc992a9b8
wenxuan-pan <panwenxuan42@gmail.com>
2023-08-22 12:43:47 +1000

    added classes page

--- src/pages/Classes.jsx
+++ src/pages/Classes.jsx
@@ -1,7 +1,33 @@
 import React from "react"
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import YearbookCard from "../components/YearbookCard"
 
 const Classes = () => {
-  return <div>Classes</div>
+  const yearbookEl = (
+    <Col md="auto" className="m-2">
+      <YearbookCard />
+    </Col>
+  )
+  const arr = [1, 2, 3, 4, 5]
+  return (
+    <Container fluid="md" className="text-md-center">
+      <Row>
+        <Col>
+          <h1>School Name</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h2 className="">All yearbooks</h2>
+        </Col>
+      </Row>
+      <Row className="justify-content-md-center">
+        {arr.map((a) => yearbookEl)}
+      </Row>
+    </Container>
+  )
 }
 
 export default Classes
