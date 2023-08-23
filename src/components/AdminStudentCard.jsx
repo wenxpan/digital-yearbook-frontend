85591b4ee530c7beeb14f1ba5a017260f8c1a08b
wenxuan-pan <panwenxuan42@gmail.com>
2023-08-23 15:22:25 +1000

    added admin manage students view

--- src/components/AdminStudentCard.jsx
+++ src/components/AdminStudentCard.jsx
@@ -8,34 +8,26 @@ import Container from "react-bootstrap/Container"
 const AdminStudentCard = () => {
   return (
     <Card style={{ maxWidth: "25rem" }} className="p-3">
-      <Card.Text>
-        <Row>
-          <Col>
-            First Name
-            <br />
-            John
-          </Col>
-          <Col>
-            Last Name
-            <br />
-            Doe
-          </Col>
-        </Row>
-      </Card.Text>
-      <Card.Text>
-        <Row>
-          <Col>
-            Email
-            <br />
-            john@gmail.com
-          </Col>
-          <Col>
-            Invite Code
-            <br />
-            fjdalkfjdklajfekl
-          </Col>
-        </Row>
-      </Card.Text>
+      <Row className="mb-2">
+        <Col>
+          <p className="fw-semibold">First Name</p>
+          <p>John</p>
+        </Col>
+        <Col>
+          <p className="fw-semibold">Last Name</p>
+          <p>Doe</p>
+        </Col>
+      </Row>
+      <Row className="mb-2">
+        <Col>
+          <p className="fw-semibold">Email</p>
+          <p>john@gmail.com</p>
+        </Col>
+        <Col>
+          <p className="fw-semibold">Invite Code</p>
+          <p>fjdalkfjdklajfekl</p>
+        </Col>
+      </Row>
       <Row className="text-center">
         <Col>
           <Button variant="danger">Delete</Button>
