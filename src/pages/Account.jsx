c8165a53958e41de537144814a5a47593c4f6b83
wenxuan-pan <panwenxuan42@gmail.com>
2023-08-31 21:41:34 +1000

    updated assets paths

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -49,7 +49,9 @@ const Account = () => {
   const currentOptions = isAdmin ? options.admin : options.student
 
   // photo to be displayed in account page
-  const accountPhoto = isAdmin ? "/src/assets/admin-default.svg" : student.photo
+  const accountPhoto = isAdmin
+    ? "./src/assets/admin-default.svg"
+    : student.photo
 
   async function handleReset() {
     // student reset their own profile
